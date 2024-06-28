b6d31d18f1d0960e7d1174ffb6bbf8a41095a885
Thomas Olenfalk <thomas@olenfalk.se>
2024-06-28 10:08:07 +0200

    update with new SOP and script

--- 4-M365DfEOnboarding.ps1
+++
@@ -1,5 +0,0 @@
-# Author: Thomas Olenfalk
-# Company: Degea AB
-# Prerequisites: Step 3 has been run (groups and guest access) and Global Administrator Role
-
-# There is no API support to configure DfE settings yet. Configure DfE manually according to the guide
